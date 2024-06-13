6c772e46eed37fe93cc153cc44d7e9d139314de3
LAPTOP-696GJNV6\hemer <hemersondamasceno90@gmail.com>
2024-06-13 20:08:13 -0300

    first commit

---
+++ Fina.Core/Responses/PagedResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Fina.Core.Responses;
+public class PagedResponse<TData>: Response<TData>
+{
+    
+    public PagedResponse(TData? data, int totalCount, int currentPage = 1, int pageSize = Configurations.DefaultPageSize)
+        : base(data)
+    {
+        Data = data;
+        CurrentPage = currentPage;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+    }
+
+    public PagedResponse(TData? data, int _statusCode = Configurations.DefaultStatusCode, string? message = null)
+        :base(data, _statusCode, message)
+    {
+    }
+
+    public int CurrentPage { get; set; }
+    public int TotalPages => (int)Math.Ceiling(TotalPages / (double)PageSize);
+    public int PageSize { get; set; } = Configurations.DefaultPageSize;
+    public int TotalCount { get; set; }
+}
---
+++ Fina.Core/Responses/Response.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fina.Core.Responses;
+public class Response<TData>
+{
+    private int _statusCode = Configurations.DefaultStatusCode;
+
+    [JsonConstructor]
+    public Response()
+    {
+        _statusCode = Configurations.DefaultStatusCode;
+    }
+
+    public Response(TData? data, int statusCode = Configurations.DefaultStatusCode, string? message = null)
+    {
+        _statusCode = statusCode;
+        Message = message;
+        Data = data;
+    }
+
+    public string? Message { get; set; }
+    public TData? Data { get; set; }
+
+    [JsonIgnore]
+    public bool IsSuccess => _statusCode is >= 200 and <= 299;
+}
